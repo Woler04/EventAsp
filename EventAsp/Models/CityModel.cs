4ceed489ec349de549056422b7a8eabc20beccde
Woler <hristo_den@abv.bg>
2022-10-26 17:43:46 +0300

    Added cities reviews and user tables in db

---
+++ EventAsp/Models/CityModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventAsp.Models
+{
+    public class CityModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
