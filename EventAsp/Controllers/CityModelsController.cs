f542aacd32e6218af36f9017789ed120472ddf89
Woler <hristo_den@abv.bg>
2022-11-10 23:05:16 +0200

    bl bla bla I cant iam dying fr

---
+++ EventAsp/Controllers/CityModelsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventAsp.Models;
+
+namespace EventAsp.Controllers
+{
+    public class CityModelsController : Controller
+    {
+        private readonly EventDbContext _context;
+
+        public CityModelsController(EventDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CityModels
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Cities.ToListAsync());
+        }
+
+        // GET: CityModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var cityModel = await _context.Cities
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cityModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(cityModel);
+        }
+
+        // GET: CityModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CityModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] CityModel cityModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cityModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cityModel);
+        }
+
+        // GET: CityModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var cityModel = await _context.Cities.FindAsync(id);
+            if (cityModel == null)
+            {
+                return NotFound();
+            }
+            return View(cityModel);
+        }
+
+        // POST: CityModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] CityModel cityModel)
+        {
+            if (id != cityModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cityModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CityModelExists(cityModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cityModel);
+        }
+
+        // GET: CityModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cities == null)
+            {
+                return NotFound();
+            }
+
+            var cityModel = await _context.Cities
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cityModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(cityModel);
+        }
+
+        // POST: CityModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Cities == null)
+            {
+                return Problem("Entity set 'EventDbContext.Cities'  is null.");
+            }
+            var cityModel = await _context.Cities.FindAsync(id);
+            if (cityModel != null)
+            {
+                _context.Cities.Remove(cityModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CityModelExists(int id)
+        {
+          return _context.Cities.Any(e => e.Id == id);
+        }
+    }
+}
